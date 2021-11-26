5e2aec3da4c007c573441ea2f1517eb14f93e28e
Jayanth Shanmugam <mk.jayanth.s@gmail.com>
2021-11-26 13:14:07 +0530

    removed comments.

--- CN_mini.py
+++ CN_mini.py
@@ -8,7 +8,6 @@ def get_App_count(net_traffic):
     app_protocols = ["HTTP","OCSP","TLSv1.2","TLSv1.3","FTP","DNS","DHCP","FTP-DATA"]
     net_app_traffic = net_traffic[net_traffic.Protocol.isin(app_protocols)]
     nt_app_proto_count = net_app_traffic.groupby('Protocol').Source.count()
-    nt_app_proto_count.columns = ['PROTOCOLS','COUNT']
     print(nt_app_proto_count)
 
 net_traffic = pd.read_csv("CN_mini_traffic.csv")
